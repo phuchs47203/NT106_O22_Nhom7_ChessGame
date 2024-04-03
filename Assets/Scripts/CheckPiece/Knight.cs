34d8539ee02c26192dbd8fa7b692ce0235a7db9c
devH <21522095@gm.uit.edu.vn>
2024-04-03 10:54:54 +0700

    Hoang commit 0304

--- Assets/Scripts/CheckPiece/Knight.cs
+++ Assets/Scripts/CheckPiece/Knight.cs
@@ -1,47 +1,11 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Knight : ChessPiece
-{
-    protected override List<Vector2Int> GetAllPossibleMove()
-    {
-        List<Vector2Int> all_Possible_Move_List = new List<Vector2Int>();
-
-        for (int x = this.currX - 2; x <= this.currX + 2; x++)
-        {
-            for (int y = this.currY - 2; y <= this.currY + 2; y++)
-            {
-                if (x == this.currX || y == this.currY)
-                {
-                    continue;
-                }
-                Vector2Int next_Move = new Vector2Int(x, y);
-                Vector2Int move_Dir = next_Move - new Vector2Int(this.currX, this.currY);
-
-                if (this.Check_Outside_The_Board(next_Move))
-                {
-                    continue;
-                }
-                if (this.Check_Being_Blocked_By_TeamAt(next_Move))
-                {
-                    continue;
-                }
-
-                if (move_Dir.x == move_Dir.y || move_Dir.x + move_Dir.y == 0)
-                {
-                    continue;
-                }
-
-                if (this.Check_Is_BlockedByOtherTeamAt(next_Move))
-                {
-                    this.capturableMoveList.Add(next_Move);
-                    continue;
-                }
-
-                all_Possible_Move_List.Add(next_Move);
-            }
-        }
-
-        return all_Possible_Move_List;
-    }
-}
+fileFormatVersion: 2
+guid: 0db6b8b9faf19e74bb4714a582cbef57
+MonoImporter:
+  externalObjects: {}
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 
--- Assets/Scripts/CheckPiece/NullPiece.cs
+++ Assets/Scripts/CheckPiece/NullPiece.cs
@@ -1,20 +1,11 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NullPiece : ChessPiece
-{
-    protected override void Awake()
-    {
-        base.Awake();
-
-        this.currX = -1;
-        this.currY = -1;
-        this.piece_Type = Chess_Piece_Type.NullPiece;
-    }
-
-    protected override List<Vector2Int> GetAllPossibleMove()
-    {
-        return new List<Vector2Int>();
-    }
-}
+fileFormatVersion: 2
+guid: e41108be198da62419e758731f5bcb70
+MonoImporter:
+  externalObjects: {}
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 
